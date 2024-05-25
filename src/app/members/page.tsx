bd5fb9e200f35c98d2ebcc05eb6c916a6e5422d5
Stacy Suits <stacyrays@gmail.com>
2024-05-25 12:11:00 -0700

    init next match app

---
+++ src/app/members/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from '@nextui-org/react'
+
+export default function MembersPage() {
+  return (
+    <div>
+      <h3 className='text-3xl'>This will be the members page</h3>
+      <Link href='/'>Go back home</Link>
+    </div>
+  )
+}
